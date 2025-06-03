4908f7405f7aa8b908678b027219506faa15ea58
ha nguyen thanh <ha.nguyenthanh@asnet.com.vn>
2025-06-03 15:35:06 +0700

    Update width and height

--- src/components/Common/TransitionLoader/index.tsx
+++ src/components/Common/TransitionLoader/index.tsx
@@ -26,7 +26,7 @@ const TransitionLoader = () => {
   return (
     <div className="fixed inset-0 z-[9999] bg-black bg-opacity-30 flex items-center justify-center">
       <div role="status">
-        <Loading />
+        <Loading width={10} height={10} />
         <span className="sr-only">Loading...</span>
       </div>
     </div>
