7a4decf730163ec90fdd52796772d1cda350f0ea
ha nguyen thanh <ha.nguyenthanh@asnet.com.vn>
2025-06-16 14:48:27 +0700

    Add 'use server'

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+'use server';
+
 import { cookies } from 'next/headers';
 
 export const getTokenFromCookies = async (): Promise<string> => {
