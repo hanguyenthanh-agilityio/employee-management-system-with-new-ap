8253fc631b466ea55d5bfc03718cf5bfcbf7f9dc
ha nguyen thanh <ha.nguyenthanh@asnet.com.vn>
2025-06-16 18:50:04 +0700

    Update validate and Ui for card

--- src/utils/schemas/authSchema.ts
+++ src/utils/schemas/authSchema.ts
@@ -1,9 +1,13 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  email: z.string().email({
-    message: 'Please enter a valid email address',
-  }),
+  email: z
+    .string()
+    .min(5, { message: 'Email is required' })
+    .max(100, { message: 'Email is too long' })
+    .email({
+      message: 'Please enter a valid email address',
+    }),
   password: z.string().min(6).max(100, {
     message: 'Password must be between 6 and 100 characters',
   }),
@@ -13,10 +17,28 @@ export type LoginInput = z.infer<typeof loginSchema>;
 
 export const registerSchema = z
   .object({
-    firstName: z.string().min(1, 'First name is required'),
-    lastName: z.string().min(1, 'Last name is required'),
+    firstName: z
+      .string()
+      .min(2, 'First name must be at least 2 characters')
+      .max(50, 'First name is too long')
+      .regex(
+        /^[a-zA-Z\s'-]+$/,
+        'First name can only contain letters, spaces, apostrophes, and hyphens',
+      ),
+    lastName: z
+      .string()
+      .min(2, 'First name must be at least 2 characters')
+      .max(50, 'First name is too long')
+      .regex(
+        /^[a-zA-Z\s'-]+$/,
+        'First name can only contain letters, spaces, apostrophes, and hyphens',
+      ),
     email: z.string().email('Invalid email'),
-    phone: z.string().min(1, 'Phone is required'),
+    phone: z
+      .string()
+      .min(8, 'Phone must be at least 8 digits')
+      .max(11, 'Phone must be at most 11 digits')
+      .regex(/^\d+$/, 'Phone must contain only digits'),
     password: z.string().min(6, 'Password must be 6+ chars'),
     confirmPassword: z.string(),
     newsletter: z.boolean().refine((val) => val === true, {
