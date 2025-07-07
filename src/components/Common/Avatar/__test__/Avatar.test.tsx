90812799ce1b9d2c16b4cc680852f1aab8f3b8df
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-07-07 17:02:01 +0700

    Update unit test

--- src/components/Common/Avatar/__test__/Avatar.test.tsx
+++ src/components/Common/Avatar/__test__/Avatar.test.tsx
@@ -4,9 +4,6 @@ import '@testing-library/jest-dom';
 // Components
 import Avatar from '..';
 
-// Constants
-import { AVATAR_URL } from '@/constants';
-
 describe('Avatar component', () => {
   test('renders fallback letter when no src is provided', () => {
     render(<Avatar name="Diana" />);
@@ -16,10 +13,5 @@ describe('Avatar component', () => {
   test('renders "?" when name is empty', () => {
     render(<Avatar name="" />);
     expect(screen.getByText('?')).toBeInTheDocument();
-  });
-
-  test('renders fallback letter when no src is provided', () => {
-    render(<Avatar name="Diana" src={AVATAR_URL} />);
-    expect(screen.getByText('D')).toBeInTheDocument();
   });
 });
