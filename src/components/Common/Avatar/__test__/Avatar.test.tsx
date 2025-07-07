344a49c58dd83b857a8c0b99adbefa2a7fd05632
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-07-07 17:00:54 +0700

    Update unit test

--- src/components/Common/Avatar/__test__/Avatar.test.tsx
+++ src/components/Common/Avatar/__test__/Avatar.test.tsx
@@ -3,6 +3,8 @@ import '@testing-library/jest-dom';
 
 // Components
 import Avatar from '..';
+
+// Constants
 import { AVATAR_URL } from '@/constants';
 
 describe('Avatar component', () => {
@@ -16,17 +18,8 @@ describe('Avatar component', () => {
     expect(screen.getByText('?')).toBeInTheDocument();
   });
 
-  test('renders image when src is provided', () => {
+  test('renders fallback letter when no src is provided', () => {
     render(<Avatar name="Diana" src={AVATAR_URL} />);
-    const image = screen.getByRole('img');
-    const src = image.getAttribute('src');
-
-    // Use decodeURIComponent() to decode the encoded string
-    const decodedUrl = decodeURIComponent(src || '');
-
-    expect(decodedUrl).toContain(
-      'images.icon-icons.com/3708/PNG/512/girl_female_woman_person_people_avatar_icon_230016.png',
-    );
-    expect(image).toHaveAttribute('alt', 'Diana');
+    expect(screen.getByText('D')).toBeInTheDocument();
   });
 });
