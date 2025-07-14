57f9eadc812ea7d83b1354ca19e0d5cc3ae976e3
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-07-14 14:50:17 +0700

    Update import type

--- src/components/Profile/ContactDetailsSection/index.tsx
+++ src/components/Profile/ContactDetailsSection/index.tsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 // types
-import { ContactsDetailsType } from '@/types';
+import { ContactsDetailsType } from '@/types/profile';
 
 // Components
 import { ContactDetailsForm } from '@/components';
