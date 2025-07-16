21ca72a2e36dfb5213db917aef5ccf8d21deb5bf
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-07-16 11:06:31 +0700

    Update unit test

--- src/components/Profile/ContactDetailsSection/__test__/ContactDetailsSection.test.tsx
+++ src/components/Profile/ContactDetailsSection/__test__/ContactDetailsSection.test.tsx
@@ -3,18 +3,24 @@ import { render, screen } from '@testing-library/react';
 import { mockContact } from '@/mocks/profile';
 import ContactDetailsSection from '..';
 
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+  }),
+}));
+
 describe('ContactDetailsSection component', () => {
   const contact = mockContact;
 
   test('Renders ContactDetailsSection inside a form element', () => {
-    render(<ContactDetailsSection contact={contact} />);
+    render(<ContactDetailsSection contact={contact} userId={2} />);
     const form = screen.getByTestId('contact-details-form');
 
     expect(form).toBeInTheDocument();
   });
 
   test('Renders from fields with default values from Contact', () => {
-    render(<ContactDetailsSection contact={contact} />);
+    render(<ContactDetailsSection contact={contact} userId={2} />);
 
     expect(screen.getByDisplayValue(contact.phoneNumber1)).toBeInTheDocument();
 
