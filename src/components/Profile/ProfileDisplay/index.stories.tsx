906738318ea3952b3da92d1f9a6dc9df9e40e42c
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-07-23 11:37:00 +0700

    Add storybook

--- src/components/Profile/ProfileDisplay/index.stories.tsx
+++ src/components/Profile/ProfileDisplay/index.stories.tsx
@@ -1,52 +1,25 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { useForm } from 'react-hook-form';
-
 // Components
-import { ProfileEditForm } from '@/components';
+import { ProfileDisplay } from '@/components';
 
-// Utils
-import { PersonalDetailsInput } from '@/utils/schemas/updateProfile';
+// Constants
+import { AVATAR_URL } from '@/constants';
 
 // Mocks
 import { mockProfile } from '@/mocks/profile';
 
-const meta: Meta<typeof ProfileEditForm> = {
-  title: 'Components/Profile/ProfileEditForm',
-  component: ProfileEditForm,
-  parameters: {
-    layout: 'centered',
-  },
+const meta: Meta<typeof ProfileDisplay> = {
+  title: 'Components/Profile/ProfileDisplay',
+  component: ProfileDisplay,
   tags: ['autodocs'],
+  args: {
+    url: AVATAR_URL,
+    profile: mockProfile,
+  },
 };
 
 export default meta;
+type Story = StoryObj<typeof meta>;
 
-type Story = StoryObj<typeof ProfileEditForm>;
-
-const avatarValue: string | null =
-  Array.isArray(mockProfile.avatar) &&
-  typeof mockProfile.avatar[0]?.url === 'string'
-    ? mockProfile.avatar[0].url
-    : null;
-
-const defaultValues: PersonalDetailsInput = {
-  username: mockProfile.username,
-  department: mockProfile.department,
-  jobTitle: mockProfile.jobTitle,
-  jobCategory: mockProfile.jobCategory,
-  documentId: undefined,
-  avatar: avatarValue,
-};
-
-const Form = () => {
-  const form = useForm<PersonalDetailsInput>({
-    defaultValues,
-  });
-
-  return <ProfileEditForm form={form} disable={false} />;
-};
-
-export const Default: Story = {
-  render: () => <Form />,
-};
+export const Default: Story = {};
