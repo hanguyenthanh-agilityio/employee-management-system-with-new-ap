c391ec8cd8e98f67dc87bfbc9e5892a0b76f735b
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-07-24 16:59:49 +0700

    Implement unit test for useProfile and useClickOutside

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,8 +1,13 @@
 import { useRouter } from 'next/navigation';
 import { useState, useTransition } from 'react';
 
+// Constants
 import { ERROR_MESSAGE } from '@/constants';
+
+// Services
 import { updateProfile } from '@/services/apiService';
+
+// Utils
 import {
   ContactDetailsInput,
   PersonalDetailsInput,
@@ -12,7 +17,10 @@ type ProfileInput = PersonalDetailsInput | ContactDetailsInput;
 
 export const useUpdateProfile = () => {
   const router = useRouter();
+  // useTransition(): Mark non-urgent tasks
   const [isPending, startTransition] = useTransition();
+
+  // State to store errors if the API returns a failure or network error
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const update = async (data: ProfileInput, userId: string) => {
@@ -20,17 +28,20 @@ export const useUpdateProfile = () => {
       const result = await updateProfile(userId, data);
 
       if (result.success) {
+        // router.refresh() is non-blocking so should be placed in startTransition()
         startTransition(() => router.refresh());
       } else {
+        // Show errors from API or default errors
         setErrorMessage(result.message ?? ERROR_MESSAGE.UNKNOWN);
       }
 
       return result;
+      // In case: fetch fail, display default error
     } catch (error) {
       setErrorMessage(ERROR_MESSAGE.UNKNOWN);
       return { success: false, message: ERROR_MESSAGE.UNKNOWN };
     }
   };
 
-  return { update, isPending, errorMessage };
+  return { update, isPending, errorMessage, setErrorMessage };
 };
