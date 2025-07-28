99be1fcfb6a2bbf4da05222368719dbd28d116f2
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-07-28 09:19:12 +0700

    Implement UT for documentid

---
+++ src/components/dashboard/index.ts
@@ -0,0 +1,4 @@
+export { default as QuickActions } from './QuickActions';
+export { default as PaySlipSection } from './PaySlipSection';
+export { default as BirthdaySection } from './BirthdaySection';
+export { default as BirthdayItem } from './BirthdayItem';
