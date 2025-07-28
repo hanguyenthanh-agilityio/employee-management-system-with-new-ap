c13ee79e44c6df38b45dec452ecbb07ef4dff58c
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-07-28 10:07:44 +0700

    Implement UT Auth

---
+++ src/app/(auth)/login/__test__/loginPage.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import LoginPage from '../page';
+
+// Mock LoginForm
+jest.mock('@/components', () => ({
+  LoginForm: () => <div data-testid="login-form">Mocked LoginForm</div>,
+}));
+
+describe('LoginPage', () => {
+  it('renders the LoginForm component', async () => {
+    // page.tsx is an server component: waits for the result of await LoginPage() before rendering
+    const Component = await LoginPage();
+    render(Component);
+
+    expect(screen.getByTestId('login-form')).toBeInTheDocument();
+
+    expect(screen.getByText('Mocked LoginForm')).toBeInTheDocument();
+  });
+});
