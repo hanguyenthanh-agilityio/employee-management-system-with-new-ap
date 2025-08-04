c0dc249aa3d5750d52d30bf5f5e61559e82026c6
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-08-04 23:04:09 +0700

    Implement Dasboard skeleton

---
+++ src/components/dashboard/DashboardWrapper/index.tsx
@@ -0,0 +1,47 @@
+// Components
+import {
+  BirthdaySection,
+  Header,
+  LeaveSection,
+  PaySlipSection,
+  ProfileSection,
+  QuickActions,
+} from '@/components';
+
+// Services
+import { getCachedUser } from '@/services';
+
+// APIs
+import { fetchSummaryLeaves } from '@/api/leaveApplications';
+
+const DashboardWrapper = async () => {
+  const user = await getCachedUser();
+  const summaryData = await fetchSummaryLeaves();
+
+  // Show user detail
+  const { username, jobTitle } = user;
+
+  return (
+    <div className="flex flex-col gap-4 md:gap-6 lg:gap-12 lg:px-8">
+      <h1 className="sr-only">Dashboard page</h1>
+      <Header title="Dashboard" />
+
+      {/* Profile Section */}
+      <ProfileSection name={username} jobTitle={jobTitle} />
+
+      {/* Quickly Action */}
+      <div>
+        <h2 className="mb-4 md:mb-6 text-2xl md:text-3xl">Quick Actions</h2>
+        <QuickActions />
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <LeaveSection data={summaryData.data} />
+        <BirthdaySection />
+        <PaySlipSection />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardWrapper;
--- src/components/status/index.ts
+++ src/components/status/index.ts
@@ -3,3 +3,4 @@ export { default as NotFoundMessage } from './NotFoundMessage';
 export { default as LoadingFormLeave } from './LoadingFormLeave';
 export { default as DeleteConfirmModal } from './DeleteConfirmModal';
 export { default as LoadingLeaveApplication } from './LoadingLeaveApplication';
+export { default as DashboardSkeleton } from './DashboardSkeleton';
