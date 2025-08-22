2965de17afed8bcfd67cfbe7f76bf3dff6dca002
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-08-22 16:32:16 +0700

    Update UT for CreateLeaveContent

--- src/components/leave/CreateLeaveContent/__test__/createLeave.test.tsx
+++ src/components/leave/CreateLeaveContent/__test__/createLeave.test.tsx
@@ -1,8 +1,15 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import CreateLeaveContent from '..';
+import { useSearchParams } from 'next/navigation';
+
+jest.mock('next/navigation', () => ({
+  useSearchParams: jest.fn(),
+}));
 
 jest.mock('@/components', () => ({
-  Breadcrumbs: () => <div data-testid="breadcrumbs" />,
+  Breadcrumbs: ({ paths }: { paths: string[] }) => (
+    <div data-testid="breadcrumbs">{JSON.stringify(paths)}</div>
+  ),
   LoadingFormLeave: () => <div data-testid="loading-form">Loading...</div>,
 }));
 
@@ -11,24 +18,38 @@ jest.mock('@/components/leave/CreateForm', () => ({
   default: () => <div data-testid="create-leave-form">Create Leave Form</div>,
 }));
 
+jest.mock('@/constants', () => ({
+  BREADCRUMBS: {
+    CREATE_LEAVE: (type: string) => [`Home`, `Leave`, `Create ${type}`],
+  },
+}));
+
 describe('CreateLeavePage', () => {
-  test.skip('renders breadcrumbs, title, description, and form', async () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('renders with default type when no type is passed', async () => {
+    (useSearchParams as jest.Mock).mockReturnValue({
+      get: () => null,
+    });
+
     render(<CreateLeaveContent />);
 
-    expect(screen.getByTestId('breadcrumbs')).toBeInTheDocument();
+    expect(screen.getByTestId('breadcrumbs')).toHaveTextContent(
+      JSON.stringify(['Home', 'Leave', 'Create Leave']),
+    );
+  });
 
-    expect(screen.getByText('Leave Application')).toBeInTheDocument();
-
-    expect(
-      screen.getByText(/Fill the required fields below to apply for leave/i),
-    ).toBeInTheDocument();
-
-    // While waiting for lazy component
-    expect(screen.getByTestId('loading-form')).toBeInTheDocument();
-
-    // Wait for lazy component to appear
-    await waitFor(() => {
-      expect(screen.getByTestId('create-leave-form')).toBeInTheDocument();
+  test('renders with custom type when type is passed', async () => {
+    (useSearchParams as jest.Mock).mockReturnValue({
+      get: () => 'Sick',
     });
+
+    render(<CreateLeaveContent />);
+
+    expect(screen.getByTestId('breadcrumbs')).toHaveTextContent(
+      JSON.stringify(['Home', 'Leave', 'Create Sick']),
+    );
   });
 });
